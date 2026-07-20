b28e5cfc3eb63730411fc4c1119ad93d5b27619b
osmedt <oliver@smedt.dk>
2026-07-20 18:15:17 +0200

    created a script for quickly creating the training data

--- streamer_net/collect_data.py
+++ streamer_net/collect_data.py
@@ -4,20 +4,26 @@ import os
 vid = cv2.VideoCapture(0)
 vid.set(3,200)
 vid.set(4,200)
-i=0
+with open('emote_training_data/categories.txt') as f:
+    categories = f.read().splitlines()
 
-while (True):
+for i, category in enumerate(categories):
+    j=0
+    print(f"current category: {category}")
+    if i!=len(categories)-1:
+        print(f"next category: {categories[i+1]}")
+    while True:
+        rect, frame = vid.read()
 
-    # inside infinity loop
-    rect, frame = vid.read()
+        if not os.path.exists('emote_training_data/'+str(i)):
+            os.makedirs('emote_training_data/'+str(i))
 
-    cv2.imwrite(f"streamer_net/emote_training_data/1/{i}.png", frame)
-    cv2.imshow('frame', frame)
+        cv2.imwrite("emote_training_data/{0}/{1}.png".format(i, j), frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    i+=1
+        j+=1
 
 vid.release()
 cv2.destroyAllWindows()
